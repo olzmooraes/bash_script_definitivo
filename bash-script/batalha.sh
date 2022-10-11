aa5d0c0f62027aef842baaf6aeed6c3c1a9433da
olzmooraes <luizotaviomoraesdacruz@gmail.com>
2022-10-10 22:03:49 -0300

    atualizacao

--- bash-script/batalha.sh
+++ bash-script/batalha.sh
@@ -25,26 +25,28 @@ vida_pkm1="${HP_pkm1[2]}"
 vida_pkm2="${HP_pkm2[2]}"
 ataque_pkm1="${attack_pkm1[2]}"
 ataque_pkm2="${attack_pkm2[2]}"
+speed_pkm1="${speed_pkm1[2]}"
+speed_pkm2="${speed_pkm2[2]}"
 
-if [[ ${speed_pkm1[2]} -gt ${speed_pkm2[2]} ]]; then
+if (( $speed_pkm1 < $speed_pkm2 )); then
 
-    while [[ $vida_pkm1 -gt 0 && $vida_pkm2 -gt 0 ]]; do
+    while [[ $vida_pkm1 > 0 && $vida_pkm2 -gt 0 ]]; do
 
-        if [[ $vida_pkm2 -gt 0 ]]; then
+        if (( $vida_pkm2 > 0 )); then
 
-            ataque_final_pkm1= $((ataque_pkm1 - $defesa_pkm2))
-            vida_pkm2= $((vida_pkm2 - $ataque_final_pkm1))
-            j=$((j + 1))
+            ataque_final_pkm1=$(($ataque_pkm1 - $defesa_pkm2))
+            vida_pkm2=$(($vida_pkm2 - $ataque_final_pkm1))
+            j=$(($j + 1))
         fi
-        if [[ $vida_pkm1 -gt 0 ]]; then
-            ataque_final_pkm2= $((ataque_pkm2 - $defesa_pkm1))
-            vida_pkm1= $((vida_pkm1 - $ataque_final_pkm2))
-            i=$((i + 1))
+        if (( $vida_pkm1 > 0 )); then
+            ataque_final_pkm2=$((ataque_pkm2 - $defesa_pkm1))
+            vida_pkm1=$((vida_pkm1 - $ataque_final_pkm2))
+            i=$(($i + 1))
         fi
         
     done
 
-    if [[ $vida_pkm1 -gt 0 ]]; then
+    if [[ $vida_pkm1 > 0 ]]; then
 
         echo "O primeiro pokemon escolhido ganhou a batalha"
         echo "Sua vida restante é $vida_pkm1"
@@ -52,15 +54,19 @@ if [[ ${speed_pkm1[2]} -gt ${speed_pkm2[2]} ]]; then
 
     fi
 
-    if [[ $vida_pkm2 -gt 0 ]]; then
+    if (( $vida_pkm2 > 0 )); then
 
         echo "O Segundo pokemon escolhido ganhou a batalha"
         echo "Sua vida restante é $vida_pkm2"
         echo "Foram necessários $i hits para derrotar o pokemon adversário"
 
     fi
+    
+    echo "funcionou"
+
+    else
+
+    echo "funcoonou o segundo"
+
 fi
-
-#if [[ ${speed_pkm2[2]} > ${speed_pkm2[2]} ]]; then
-
-#fi
+ 
--- bash-script/teste.sh
+++ bash-script/teste.sh
@@ -1,13 +1,16 @@
 #! /bin/bash
 
-var=500
-var2=5001
-#if [[ $var > $var2 ]]; then
-    echo "funcionou"
-#fi
-#if [[ $var <= $var2 ]]; then
-    echo "funcionou"
-#fi
-if [[ $var -lt $var2 ]]; then
-    echo "funcionou denovo"
-fi
+cd ./pokemon
+
+attack1_pkm1=$(grep "Attack" ./pkm1.txt)
+read -a attack_pkm1 <<<"$attack1_pkm1"
+attack2_pkm2=$(grep "Attack" ./pkm2.txt)
+read -a attack_pkm2 <<<"$attack2_pkm2"
+
+defesa_pkm2="${id2[5]}"
+ataque_pkm1="${attack_pkm1[2]}"
+
+
+ ataque_final_pkm1=$(( $ataque_pkm1 - $defesa_pkm2 ))
+
+ echo $ataque_final_pkm1
